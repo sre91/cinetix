405248e60c38a62a2532517cde852a51e09f6882
sre91 <sreems91@gmail.com>
2025-10-16 12:36:36 +0530

    Removed submodule and fixed client folder

---
+++ client/src/App.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
